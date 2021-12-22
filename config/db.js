a7986be49669fd5696ef3f7d37d5af6e5b16bf12
unknown <mirzatalha.1998@gmail.com>
2021-12-22 12:58:27 +0500

    mongo url

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const mongoose=require("mongoose");
 const dotenv= require('dotenv').config();
 
 const connectDB=async()=>{
-    mongoose.connect(process.env.db_url,
+    mongoose.connect(process.env.db_url || 'mongodb+srv://new1:new12@cluster0.uyoov.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
     {
         useNewUrlParser:true,
     })
